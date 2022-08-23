dae75660cc058ce45c877fa51cb5590bb26bda32
mehulg <mehul.gohil@killiq.co.in>
2022-08-23 09:55:13 +0530

    commit

--- angular & firebase/firebase-crud/src/app/pages/forgot-password/forgot-password.component.ts
+++ angular & firebase/firebase-crud/src/app/pages/forgot-password/forgot-password.component.ts
@@ -4,15 +4,14 @@ import { AuthService } from 'src/app/shared/auth.service';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.css']
+  styleUrls: ['./forgot-password.component.css'],
 })
 export class ForgotPasswordComponent implements OnInit {
-email:string ='';
-  constructor(private fireauth:AuthService) { }
+  email: string = '';
+  constructor(private fireauth: AuthService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  forgotpassword() {
+    this.fireauth.forgotpassword(this.email);
   }
-forgotpassword(){
-    this.fireauth.forgotpassword(this.email)
-}
 }
